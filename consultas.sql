1f2becf8c810c0ccd62d9ee418d2f8720bdc33df
Marc Boreu <34090642+marcboreu@users.noreply.github.com>
2020-07-01 23:07:29 +0200

    kickoff2

--- consultas.sql
+++ consultas.sql
@@ -7,42 +7,29 @@ SELECT COUNT(*) FROM flights;
 
 /*2. Retraso promedio de salida y llegada según el aeropuerto origen.  */
 
-SELECT origin,
-AVG (deptime) AS promedio_salida,
-AVG (arrtime) AS promedio_llegada
+SELECT origin, AVG (deptime) AS promedio_salida, AVG (arrtime) AS promedio_llegada
 FROM flights
 GROUP BY origin;
 
 /*3. Retraso promedio de llegada de vuelos, por meses, años y según aeropuerto de origen. Mostrar resultados DESC */
 
-SELECT origin, colMonth, colYear, 
-AVG (arrtime) AS promedio_llegada
+SELECT origin, colYear, colMonth, AVG (arrtime) AS promedio_llegada
 FROM flights
-GROUP BY origin ASC;
+ORDER BY origin ASC, colYear ASC, colMonth ASC;
 
 /*4. Misma consulta que antes, pero mostrar ciudad en vez de código de aeropuerto   */
 
-SELECT * 
-FROM usairports;
-SELECT DISTINCT city, ColMonth, ColYear,
-AVG (ArrTime) AS promedio_llegada
-FROM flights
-JOIN usairports
-ON  flights.flightid = usairports.iata  
-GROUP BY city ASC; 
+SELECT AIR.city, colYear, colMonth, AVG (ArrTime) AS promedio_llegada
+FROM flights AS FL, usairports AS 
+WHERE FL.Origin = usairports.iata  
+GROUP BY AIR.City ASC, FL.colYear ASC, FL.col.Month ASC; 
 
-/*5. 5. Las compañías con más vuelos cancelados, por meses y año. Además, deben estar ordenadas de forma que las compañías con más cancelaciones aparezcan las primeras.  */
+/*5. Las compañías con más vuelos cancelados, por meses y año. Además, deben estar ordenadas de forma que las compañías con más cancelaciones aparezcan las primeras.  */
 
-SELECT * 
-FROM flights;
-SELECT * 
-FROM carriers;
 SELECT carriercode, colmonth, colyear, cancelled AS total_cancelados
 FROM flights
-JOIN carriers
-ON flights.flightId = carriers.carriercode
-GROUP BY description
-ORDER BY cancelled DESC;
+GROUP BY carriercode
+ORDER BY cancelled DESC, colyear ASC, colMonth ASC;
 
 /* 6. El identificador de los 10 aviones que más distancia han recorrido haciendo vuelos. */
 
@@ -54,11 +41,8 @@ LIMIT 10 OFFSET 1;
 
 /* 7. Compañías con su retraso promedio (mayor de 10 minutos) */
 
-SELECT * 
-FROM flights;
-SELECT carriercode, description, AVG(arrdelay) AS retraso_promedio
+SELECT carriercode , AVG(arrdelay) AS retraso_promedio
 FROM flights
-JOIN carriers
-ON flights.flightId = carriers.carriercode
-WHERE arrdelay > 10
+GROUP BY carriercode
+HAVING arrdelay > 10
 GROUP BY arrdelay DESC;
