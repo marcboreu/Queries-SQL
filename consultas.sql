574c00d94ab5b5594b49caecedf17bfcdec9dd89
Marc Boreu <34090642+marcboreu@users.noreply.github.com>
2020-07-29 11:04:05 +0200

    Update consultas.sql
    kick off 4

--- consultas.sql
+++ consultas.sql
@@ -1,48 +1,59 @@
 /* 1. Cantidad de registros de la tabla de vuelos: Resultado: TOTAL 4758 */
-SHOW DATABASES;
-use USAirlineFlights2;
-SELECT * 
-FROM flights;
-SELECT COUNT(*) FROM flights;
+SELECT COUNT(*)
+FROM Flights;
 
 /*2. Retraso promedio de salida y llegada según el aeropuerto origen.  */
 
-SELECT origin, AVG (deptime) AS promedio_salida, AVG (arrtime) AS promedio_llegada
-FROM flights
-GROUP BY origin;
+SELECT Origin, AVG(ArrDelay), AVG(DepDelay)
+FROM Flights
+GROUP BY Origin;
 
 /*3. Retraso promedio de llegada de vuelos, por meses, años y según aeropuerto de origen. Mostrar resultados DESC */
 
-SELECT origin, colYear, colMonth, AVG (arrtime) AS promedio_llegada
-FROM flights
-ORDER BY origin ASC, colYear ASC, colMonth ASC;
+SELECT Origin, colYear, colMonth, AVG(ArrDelay)
+FROM Flights
+GROUP BY Origin, colYear, colMonth
+ORDER BY Origin, colYear, colMonth;
 
 /*4. Misma consulta que antes, pero mostrar ciudad en vez de código de aeropuerto   */
 
-SELECT AIR.city, colYear, colMonth, AVG (ArrTime) AS promedio_llegada
-FROM flights AS FL, usairports AS 
-WHERE FL.Origin = usairports.iata  
-ORDER BY AIR.City ASC, FL.colYear ASC, FL.col.Month ASC; 
+SELECT AIRP.City, FL.colYear, FL.colMonth, AVG(FL.ArrDelay)
+FROM Flights as FL
+LEFT JOIN USAirports as AIRP
+ON FL.Origin = AIRP.IATA
+GROUP BY City, colYear, colMonth
+ORDER BY City, colYear, colMonth;
 
 /*5. Las compañías con más vuelos cancelados, por meses y año. Además, deben estar ordenadas de forma que las compañías con más cancelaciones aparezcan las primeras.  */
 
-SELECT carriercode, colmonth, colyear, cancelled AS total_cancelados
-FROM flights
-GROUP BY carriercode
-ORDER BY cancelled DESC, colyear ASC, colMonth ASC;
+SELECT UniqueCarrier, colYear, colMonth, SUM(Cancelled) as total_cancelados
+FROM Flights
+GROUP BY UniqueCarrier, colYear, colMonth
+HAVING total_cancelados > 0
+ORDER BY total_cancelados DESC, colYear ASC, colMonth ASC
+o
+SELECT UniqueCarrier, colYear, colMonth, SUM(Cancelled)
+FROM Flights
+INNER JOIN USAirports
+ON Origin = IATA
+GROUP BY UniqueCarrier, colYear, colMonth
+HAVING SUM(Cancelled) > 0
+ORDER BY SUM(Cancelled) DESC;
 
 /* 6. El identificador de los 10 aviones que más distancia han recorrido haciendo vuelos. */
 
-SELECT tailnum AS identificador_avion, SUM(distance) AS total_distance
-FROM flights
-WHERE tailnum IS NOT NULL
-GROUP BY tailnum
-LIMIT 10 OFFSET 1;
+SELECT TailNum, SUM(Distance) as totalDistance
+FROM Flights
+WHERE TailNum != "NA" and TailNum != "#?NOMBRE?"
+GROUP BY TailNum
+ORDER BY totalDistance DESC
+Limit 10;
 
 /* 7. Compañías con su retraso promedio (mayor de 10 minutos) */
 
-SELECT carriercode , AVG(arrdelay) AS retraso_promedio
-FROM flights
-GROUP BY carriercode
-HAVING arrdelay > 10
-GROUP BY arrdelay DESC;
+
+SELECT UniqueCarrier, avg(ArrDelay) as avgDelay
+FROM Flights
+GROUP BY UniqueCarrier
+HAVING avgDelay > 10.0
+ORDER BY avgDelay DESC;
